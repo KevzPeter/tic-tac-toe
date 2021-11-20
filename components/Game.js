5d946b9503a07303eba7af27b7639c11d8e8352f
KevzPeter <ultimatekevin1@gmail.com>
2021-11-20 15:03:00 +0530

    feat: stats, fixed mechanics

--- components/Game.js
+++ components/Game.js
@@ -19,6 +19,8 @@ const lines = [
 const Game = () => {
   const [squares, setSquares] = useState(defaultSquares());
   const [winner, setWinner] = useState(null);
+  const [userwins, setUserwins] = useState(0);
+  const [computerwins, setComputerwins] = useState(0);
 
   useEffect(() => {
     const isComputerTurn = squares.filter((square) => square !== null).length % 2 == 1 && squares.filter((square) => square !== null).length <= 8;
@@ -34,13 +36,13 @@ const Game = () => {
     const playerWon = linesThatAre("x", "x", "x").length > 0;
     const computerWon = linesThatAre("o", "o", "o").length > 0;
 
-    if (playerWon) {
+    if (playerWon && winner !== "Draw" && winner !== "o") {
       setWinner("x");
-      //setTimeout(resetGame(), 3000);
+      setUserwins(userwins + 1);
     }
-    if (computerWon) {
+    if (computerWon && winner !== "Draw" && winner !== "x") {
       setWinner("o");
-      //setTimeout(resetGame(), 3000);
+      setComputerwins(computerwins + 1);
     }
     const putComputerAt = (index) => {
       setTimeout(() => {}, 3000);
@@ -76,7 +78,7 @@ const Game = () => {
   }, [squares]);
 
   const handleSquareClick = (index) => {
-    if (squares[index] == null) {
+    if (winner == null && squares[index] == null) {
       const isPlayerTurn = squares.filter((square) => square !== null).length % 2 === 0;
       if (isPlayerTurn) {
         let newSquares = squares;
@@ -84,31 +86,61 @@ const Game = () => {
         setSquares([...newSquares]);
       }
     }
+    if (squares.filter((square) => square !== null).length == 9) setWinner("Draw");
+  };
+  const resetGame = () => {
+    setWinner(null);
+    setSquares(defaultSquares());
+  };
+
+  const ShowReset = () => {
+    return (
+      <div className={styles.result} id={styles.black} onClick={() => resetGame()}>
+        New Game 😝
+      </div>
+    );
+  };
+  const PlayerStats = () => {
+    return (
+      <div className={styles.stats}>
+        <div>🧑🏽{userwins / 2}</div>
+        <div>💻{computerwins}</div>
+      </div>
+    );
+  };
+  const ShowResult = () => {
+    return (
+      <div>
+        {winner === "Draw" && (
+          <div className={styles.result} id={styles.white}>
+            Game Drawn 😐
+          </div>
+        )}
+        {!!winner && winner === "x" && (
+          <div className={styles.result} id={styles.green}>
+            You Won 🥳
+          </div>
+        )}
+        {!!winner && winner === "o" && (
+          <div className={styles.result} id={styles.red}>
+            Computer Won 😢
+          </div>
+        )}
+        {(winner === "Draw" || winner === "x" || winner == "o") && ShowReset()}
+      </div>
+    );
   };
 
   return (
     <div className={styles.main}>
       <h1>Tic Tac Toe</h1>
+      <PlayerStats />
       <Board>
         {squares.map((square, index) => (
           <Square key={index} x={square === "x" ? 1 : 0} o={square === "o" ? 1 : 0} onClick={() => handleSquareClick(index)} />
         ))}
       </Board>
-      {squares.filter((square) => square !== null).length == 9 && (
-        <div className={styles.result} id={styles.white}>
-          Game Drawn 😐
-        </div>
-      )}
-      {!!winner && winner === "x" && (
-        <div className={styles.result} id={styles.green}>
-          You Won 🥳
-        </div>
-      )}
-      {!!winner && winner === "o" && (
-        <div className={styles.result} id={styles.red}>
-          Computer Won 😢
-        </div>
-      )}
+      <ShowResult />
     </div>
   );
 };
